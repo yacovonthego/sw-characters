1edaeb25eef7147caadc3a5e3333c7a3bc16fdad
yakov <yakovonthego@gmail.com>
2019-09-17 12:14:15 +0300

    Search layout update.

--- src/components/search.vue
+++ src/components/search.vue
@@ -2,7 +2,9 @@
   <div>
     <div class="container">
       <div class="search">
-        <input type="text" class="search__input" placeholder="Search by name">
+        <form class="search__form">
+          <input autofocus maxlength="70" type="text" class="search__input" placeholder="Search by name">
+        </form>
       </div>
     </div>
   </div>
@@ -22,7 +24,27 @@
     padding-top: 100px
     padding-bottom: 90px
     &__form
-      width: 60%
+      width: 70%
+      border-bottom: 1px solid #808080
     &__input
-      width: 60%
+      width: 100%
+      height: 30px
+      background-color: transparent
+      color: #808080
+      border: none
+      font-family: 'Roboto', sans-serif
+      font-size: 18px
+      font-weight: 500
+      transition: 0.2s
+      background-image: url('../assets/images/search.png')
+      background-repeat: no-repeat
+      background-position: 98% 0
+      &::placeholder
+        opacity: 1
+      &:focus
+        padding-bottom: 5px
+        transition: 0.2s
+  @media (max-width: 768px)
+    .search__form
+      width: 85%
 </style>
